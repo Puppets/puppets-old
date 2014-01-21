c3487ee7063edf907820fabceafc3edfd46db06f
Jmeas <jellyes2@gmail.com>
2014-01-21 10:14:49 -0500

    Some updates

--- src/puppets.stateful.js
+++ src/puppets.stateful.js
@@ -1,63 +1,66 @@
 /*
- * 
- * Set up the states for the puppet & the stateful API
+ *
+ * Stateful
+ * Gives an object four state variables
  *
  */
 
- _.extend( window.Puppets.Puppet.prototype, {
+ var window.Stateful = {
+
+  _currentState: {},
+
+  _defaultState: 'stopped',
 
   _states: {
-    _stoppedState: {
-      started: false,
-      ready: false,
-      stopped: true,
+    stopped: {
+      started:  false,
+      ready:    false,
+      stopped:  true,
       stopping: false
     },
 
-    _stoppingState: {
-      started: true,
-      ready: false,
+    stopping: {
+      started:  true,
+      ready:    false,
       stopping: true,
-      stopped: false
+      stopped:  false
     },
 
-    _readyState: {
-      started: true,
-      ready: true,
+    ready: {
+      started:  true,
+      ready:    true,
       stopping: false,
-      stopped: false
+      stopped:  false
     },
 
-    _startedState: {
-      started: true,
-      ready: false,
+    started: {
+      started:  true,
+      ready:    false,
       stopping: false,
-      stopped: false
+      stopped:  false
     }
   },
 
-  defaultState: 'stopped',
-
-  _setDefaultState: function() {
+  setDefaultState: function() {
     this._changeState( this.defaultState );
   },
 
-  _changeState: function( state ) {
-    var newState = this._states[ '_' + state + 'State' ];
-    _.extend(this, newState);
+  changeState: function( state ) {
+    var newState = this._states[ state ];
+    this._currentState = newState;
   },
 
-  _isStarted: function() {
-    return this.started;
+  isStarted: function() {
+    return this._currentState.started;
   },
-  _isStopped: function() {
-    return this.stopped;
+  isStopped: function() {
+    return this._currentState.stopped;
   },
-  _isReady: function() {
-    return this.ready;
+  isReady: function() {
+    return this._currentState.ready;
   },
-  _isStopping: function() {
-    return this.stopping;
+  isStopping: function() {
+    return this._currentState.stopping;
   }
 
  });
